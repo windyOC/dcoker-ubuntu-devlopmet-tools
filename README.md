f8d4df0f49dce31f6f532b3c05c118853d6e989d
alast <8317786+alast@user.noreply.gitee.com>
2021-03-02 18:38:00 +0800

    User security update

--- README.md
+++ README.md
@@ -4,8 +4,16 @@
 基于Ubuntu镜像搭建基础开发环境
 
 #### 使用方法
+- 直接使用root用户
 
-`docker run -d -p 5472:22 -e password="wesode123" windyoc/ubuntu-development`
+         docker run -d -p 5472:22 -e user="root" password="wesode123" windyoc/ubuntu-development
+
+- 使用其他（test）用户
+
+        docker run -d -p 5472:22 -e user="test" password="wesode123" windyoc/ubuntu-development
 
 初始密码：wesode123
+
 初始端口：5472
+
+PS:使用其他用户时sudo命令可用
--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 mkdir /var/run/sshd
-echo "root:$password" | chpasswd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config   
-#change root password && enable root remote ssh
+useradd -ms /bin/bash "$user"
+echo "$user:$password" | sudo chpasswd && echo "Password setting success"
+if [ "$user"=="root" ]; then
+    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+else
+    echo "$user    ALL=(ALL)    ALL" >> /etc/sudoers && echo "Sudoer add success"
+fi
+#echo "PermitRootLogin yes" >> /etc/ssh/sshd_config           
+#if the user is root then it will change root password && enable root remote ssh
+#else enable this user to use "sudo" command
 /usr/sbin/sshd -D
 #run openssh-server
